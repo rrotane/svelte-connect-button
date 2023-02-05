1ad3e35ff5bfd8c3b92d663a8e272048ce0211af
kai <kai.hemme-unger@web.de>
2023-02-05 15:49:30 +0100

    css button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,7 +65,8 @@
     }
 
     radix-connect-button {
-        position: static;
+        position: absolute;
+        right: 10px;
         margin-top: 10px;
         margin-left: 10px;
         margin-right: 10px;
